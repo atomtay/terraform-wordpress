8a2c7c211ba80217d21759676ab6d8de6f8acff4
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2024-07-27 14:39:17 +0000

    Create public/private subnets in each availability zone

---
+++ network.tf
@@ -0,0 +1,22 @@
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  count                   = length(data.aws_availability_zones.available.names)
+  cidr_block              = "10.0.${count.index}.0/24"
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+}
+
+resource "aws_subnet" "private" {
+  count             = length(data.aws_availability_zones.available.names)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.${count.index + length(data.aws_availability_zones.available.names)}.0/24"
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+}
