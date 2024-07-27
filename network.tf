bb89c2846284a028da5f058149a07495dbc424c8
Annabelle Thomas Taylor <annabelle.t.taylor@gmail.com>
2024-07-27 14:57:23 +0000

    Add route table associations, re-organize file

--- network.tf
+++ network.tf
@@ -6,6 +6,27 @@ resource "aws_vpc" "main" {
   cidr_block = "10.0.0.0/16"
 }
 
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+}
+
+## NOTE: api error Resource.AlreadyAssociated with expected vpc
+## Commenting out for visibility and troubleshooting
+# resource "aws_internet_gateway_attachment" "gw_attachment" {
+#   internet_gateway_id = aws_internet_gateway.gw.id
+#   vpc_id              = aws_vpc.main.id
+# }
+
+resource "aws_route_table" "main" {
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+
+## Public subnets and route table associations
 resource "aws_subnet" "public" {
   vpc_id                  = aws_vpc.main.id
   count                   = length(data.aws_availability_zones.available.names)
@@ -14,9 +35,23 @@ resource "aws_subnet" "public" {
   map_public_ip_on_launch = true
 }
 
+resource "aws_route_table_association" "public_association" {
+  count          = length(aws_subnet.public)
+  subnet_id      = aws_subnet.public.*.id[count.index]
+  route_table_id = aws_route_table.main.id
+}
+
+## Private subnets and route table associations
+
 resource "aws_subnet" "private" {
   count             = length(data.aws_availability_zones.available.names)
   vpc_id            = aws_vpc.main.id
   cidr_block        = "10.0.${count.index + length(data.aws_availability_zones.available.names)}.0/24"
   availability_zone = data.aws_availability_zones.available.names[count.index]
+}
+
+resource "aws_route_table_association" "private_association" {
+  count          = length(aws_subnet.private)
+  subnet_id      = aws_subnet.private.*.id[count.index]
+  route_table_id = aws_route_table.main.id
 }
